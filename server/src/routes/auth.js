aedfdd60ad4bf430033c33006b1239b684003ef6
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-04-01 14:33:12 +0300

    project setup, signing teacher works

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,15 +1,61 @@
 import express from 'express';
 import FabricCAServices from 'fabric-ca-client';
+import {Gateway, InMemoryWallet, X509WalletMixin} from 'fabric-network';
 import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
 const router = express.Router();
 const studentRegistration = async (req, res) => {
-  let caInfo = yaml.safeLoad(fs.readFileSync('../gateway/fabric-ca-client-config.yaml', 'utf8'));
-  const caTLSCACerts = caInfo.tlsCACerts.pem;
-  const ca = new FabricCAServices(caInfo.url, { trustedRoots: caTLSCACerts, verify: false }, caInfo.caName);
+  const {login, password} = req.body;
+
+  const ca = new FabricCAServices('http://0.0.0.0:7054');
+  const adminData = await ca.enroll({enrollmentID: 'admin', enrollmentSecret: 'password'});
+  const identity = {
+    label: 'client',
+    certificate: adminData.certificate,
+    privateKey: adminData.key.toBytes(),
+    mspId: 'NAUKMA'
+  };
+  const wallet = new InMemoryWallet();
+  const mixin = X509WalletMixin.createIdentity(identity.mspId, identity.certificate, identity.privateKey);
+  await wallet.import(identity.label, mixin);
+  const gateway = new Gateway();
+  const connectionProfile = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname,'../gateway/networkConnection.yaml'), 'utf8'));
+  const connectionOptions = {
+    identity: identity.label,
+    wallet: wallet,
+    discovery: {enable: false, asLocalhost: true}
+  };
+  await gateway.connect(connectionProfile, connectionOptions);
+  const admin = gateway.getCurrentIdentity();
+  await ca.register({
+    enrollmentID: login,
+    enrollmentSecret: password,
+    role: 'peer',
+    affiliation: 'naukma.teacher',
+    maxEnrollments: -1
+  }, admin);
+
+  const userData = await ca.enroll({
+    enrollmentID: login,
+    enrollmentSecret: password
+  });
+  gateway.disconnect();
+  res.status(201).json(
+      {
+        login: login,
+        certificate: userData.certificate,
+        privateKey: userData.key.toBytes()
+      }
+  );
 };
 router.post('/student', studentRegistration);
+
+router.get('/student', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /transcripts'
+  });
+});
 
 export default router;
