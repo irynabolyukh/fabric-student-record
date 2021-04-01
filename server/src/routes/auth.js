219a24aae3a78728b79dfede1e50a9616fda6045
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-04-01 14:37:16 +0300

    teacher and student added, hanndling errors - needs to be done

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -33,6 +33,59 @@ const studentRegistration = async (req, res) => {
     enrollmentID: login,
     enrollmentSecret: password,
     role: 'peer',
+    affiliation: 'naukma.student',
+    maxEnrollments: -1
+  }, admin);
+
+  const userData = await ca.enroll({
+    enrollmentID: login,
+    enrollmentSecret: password
+  });
+  gateway.disconnect();
+  res.status(201).json(
+      {
+        login: login,
+        certificate: userData.certificate,
+        privateKey: userData.key.toBytes()
+      }
+  );
+};
+
+router.post('/student', studentRegistration);
+
+router.get('/student', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /student'
+  });
+});
+
+const teacherRegistration = async (req, res) => {
+  const {login, password} = req.body;
+
+  const ca = new FabricCAServices('http://0.0.0.0:7054');
+  const adminData = await ca.enroll({enrollmentID: 'admin', enrollmentSecret: 'password'});
+  const identity = {
+    label: 'client',
+    certificate: adminData.certificate,
+    privateKey: adminData.key.toBytes(),
+    mspId: 'NAUKMA'
+  };
+  const wallet = new InMemoryWallet();
+  const mixin = X509WalletMixin.createIdentity(identity.mspId, identity.certificate, identity.privateKey);
+  await wallet.import(identity.label, mixin);
+  const gateway = new Gateway();
+  const connectionProfile = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname,'../gateway/networkConnection.yaml'), 'utf8'));
+  const connectionOptions = {
+    identity: identity.label,
+    wallet: wallet,
+    discovery: {enable: false, asLocalhost: true}
+  };
+  await gateway.connect(connectionProfile, connectionOptions);
+  const admin = gateway.getCurrentIdentity();
+  await ca.register({
+    enrollmentID: login,
+    enrollmentSecret: password,
+    role: 'peer',
     affiliation: 'naukma.teacher',
     maxEnrollments: -1
   }, admin);
@@ -50,11 +103,12 @@ const studentRegistration = async (req, res) => {
       }
   );
 };
-router.post('/student', studentRegistration);
 
-router.get('/student', (req, res, next) => {
+router.post('/teacher', teacherRegistration);
+
+router.get('/teacher', (req, res, next) => {
   res.status(200).json({
-    message: 'Handling GET requests to /transcripts'
+    message: 'Handling GET requests to /teacher'
   });
 });
 
