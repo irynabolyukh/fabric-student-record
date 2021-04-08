9f86717e8e427f511e5eba5775fa326dc6a2491d
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-04-08 19:05:35 +0300

    check if record exists, add new grade for theme to record

--- chaincode/src/contracts/student-records-storage.js
+++ chaincode/src/contracts/student-records-storage.js
@@ -14,9 +14,12 @@ class StudentRecordsStorage extends Contract {
       throw new Error("Current user does not have access to this function.");
     }
     const recordAsBytes = await ctx.stub.getState(studentEmail);
-    // if(!!recordAsBytes || recordAsBytes.toString().length !== 0){
-    //   throw new Error("Student with current email already exists.");
-    // }
+    // recordAsBytes = {"type":"Buffer","data":[]}
+    if(!!recordAsBytes){ //if not null
+      if(recordAsBytes.toString().length !== 0) { //if not empty
+        throw new Error("Student with current email already exists.");
+      }
+    }
     const recordExample = {
       fullName: fullName,
       semesters: []
@@ -32,12 +35,39 @@ class StudentRecordsStorage extends Contract {
       throw new Error("Current user does not have access to this function.");
     }
     const recordAsBytes = await ctx.stub.getState(studentEmail);
+    if(!recordAsBytes || recordAsBytes.toString().length === 0) { //if null or empty
+        throw new Error("Student with current email does not exist.");
+    }
     const recordAsObject = JSON.parse(recordAsBytes.toString());
     recordAsObject.semesters[semesterNumber][subjectName] = {
       lector: identity.cert.subject.commonName,
       themes: []
     }
+    const newRecordInBytes = Buffer.from(JSON.stringify(recordAsObject));
+    await ctx.stub.putState(studentEmail, newRecordInBytes);
+    return JSON.stringify(recordExample,null,2);
+  }
 
+  async addGradeToStudentRecord(ctx, studentEmail, semesterNumber, subjectName, themeTitle, themeRating) {
+    const identity = new ClientIdentity(ctx.stub);
+    if(identity.cert.subject.organizationalUnitName !== 'admin'){
+      throw new Error("Current user does not have access to this function.");
+    }
+    const recordAsBytes = await ctx.stub.getState(studentEmail);
+    if(!recordAsBytes || recordAsBytes.toString().length === 0) { //if null or empty
+      throw new Error("Student with current email does not exist.");
+    }
+    const recordAsObject = JSON.parse(recordAsBytes.toString());
+    recordAsObject.semesters[semesterNumber][subjectName] = {
+      lector: identity.cert.subject.commonName,
+      themes: [
+        {
+          title: themeTitle,
+          rating: themeRating,
+          date: Date.now()
+        }
+      ]
+    }
     const newRecordInBytes = Buffer.from(JSON.stringify(recordAsObject));
     await ctx.stub.putState(studentEmail, newRecordInBytes);
     return JSON.stringify(recordExample,null,2);
