c5dc98e6b5468c7731b342405f1d7072dcce2a99
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-04-20 15:09:35 +0300

    record functions added

--- server/src/routes/record.js
+++ server/src/routes/record.js
@@ -17,7 +17,7 @@ const createStudentRecord = async (req, res) => {
           props: [studentEmail, studentFullName]
       });
       gateway.disconnect();
-      res.status(201).json({data:result});
+      res.status(201).json({ data: result });
     }
     catch (e) {
         res.status(400).json({ message: e.message });
@@ -34,18 +34,150 @@ const getStudentData = async (req, res) => {
         );
         const gateway = await getConnectedWallet('Org1MSP', mixin);
         const result = await sendTransaction(gateway,{
-            name: 'getStudentData',
+            name: 'getStudentRecord',
             props: [studentEmail]
         });
         gateway.disconnect();
-        res.status(201).json({data:result});
+        res.status(201).json({ data: result });
     }
     catch (e) {
         res.status(400).json({ message: e.message });
     }
 };
 
-router.post('/', createStudentRecord);
+const addSubjectToStudentRecord = async (req, res) => {
+    const {certificate, privateKey, studentEmail, semesterNumber, subjectName} = req.body;
+    try {
+        const mixin = X509WalletMixin.createIdentity(
+            'Org1MSP',
+            certificate,
+            privateKey
+        );
+        const gateway = await getConnectedWallet('Org1MSP', mixin);
+        const result = await sendTransaction(gateway,{
+            name: 'addSubjectToStudentRecord',
+            props: [studentEmail, semesterNumber, subjectName]
+        });
+        gateway.disconnect();
+        res.status(201).json({ data: result });
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
+
+const addGradeToStudentRecord = async (req, res) => {
+    const {certificate, privateKey, studentEmail, semesterNumber, subjectName, themeTitle, themeRating} = req.body;
+    try {
+        const mixin = X509WalletMixin.createIdentity(
+            'Org1MSP',
+            certificate,
+            privateKey
+        );
+        const gateway = await getConnectedWallet('Org1MSP', mixin);
+        const result = await sendTransaction(gateway,{
+            name: 'addGradeToStudentRecord',
+            props: [studentEmail, semesterNumber, subjectName, themeTitle, themeRating]
+        });
+        gateway.disconnect();
+        res.status(201).json({ data: result });
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
+
+const getAllStudentGrades = async (req, res) => {
+    const {certificate, privateKey, studentEmail} = req.body;
+    try {
+        const mixin = X509WalletMixin.createIdentity(
+            'Org1MSP',
+            certificate,
+            privateKey
+        );
+        const gateway = await getConnectedWallet('Org1MSP', mixin);
+        const result = await sendTransaction(gateway,{
+            name: 'getAllStudentGrades',
+            props: [studentEmail]
+        });
+        gateway.disconnect();
+        res.status(201).json({ data: result });
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
+
+const getAllStudentGradesForSemester = async (req, res) => {
+    const {certificate, privateKey, studentEmail, semester} = req.body;
+    try {
+        const mixin = X509WalletMixin.createIdentity(
+            'Org1MSP',
+            certificate,
+            privateKey
+        );
+        const gateway = await getConnectedWallet('Org1MSP', mixin);
+        const result = await sendTransaction(gateway,{
+            name: 'getAllStudentGradesForSemester',
+            props: [studentEmail, semester]
+        });
+        gateway.disconnect();
+        res.status(201).json({ data: result });
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
+
+const getAverageStudentGradeForSubjectInSemester = async (req, res) => {
+    const {certificate, privateKey, studentEmail, semester, subjectName} = req.body;
+    try {
+        const mixin = X509WalletMixin.createIdentity(
+            'Org1MSP',
+            certificate,
+            privateKey
+        );
+        const gateway = await getConnectedWallet('Org1MSP', mixin);
+        const result = await sendTransaction(gateway,{
+            name: 'getAverageStudentGradeForSubjectInSemester',
+            props: [studentEmail, semester, subjectName]
+        });
+        gateway.disconnect();
+        res.status(201).json({ data: result });
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
+
+const getAverageStudentGradeForSemester = async (req, res) => {
+    const {certificate, privateKey, studentEmail, semester} = req.body;
+    try {
+        const mixin = X509WalletMixin.createIdentity(
+            'Org1MSP',
+            certificate,
+            privateKey
+        );
+        const gateway = await getConnectedWallet('Org1MSP', mixin);
+        const result = await sendTransaction(gateway,{
+            name: 'getAverageStudentGradeForSemester',
+            props: [studentEmail, semester]
+        });
+        gateway.disconnect();
+        res.status(201).json({ data: result });
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+};
+
+router.post('/add', createStudentRecord);
+router.post('/subject/add', addSubjectToStudentRecord);
+router.post('/grade/add', addGradeToStudentRecord);
 router.get('/', getStudentData);
+router.get('/grades', getAllStudentGrades);
+router.get('/grades/semester', getAllStudentGradesForSemester);
+router.get('/grades/semester/average/themes', getAverageStudentGradeForSubjectInSemester);
+router.get('/grades/semester/average', getAverageStudentGradeForSemester);
 
 export default router;
